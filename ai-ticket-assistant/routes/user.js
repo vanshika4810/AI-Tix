8ca6015eb7c7ba2d53c6867dc8edcfdc22cea613
vanshika4810 <vanshikalal365@gmail.com>
2025-06-05 20:51:34 +0530

    routes created in the user.js for getting the ticket(s) or creating them

--- ai-ticket-assistant/routes/user.js
+++ ai-ticket-assistant/routes/user.js
@@ -1,5 +1,11 @@
 import express from "express";
+import authentication, { authenticate } from "../middlewares/auth";
+import { createTicket, getTicket, getTickets } from "../controllers/ticket";
 
 const router = express.Router();
+
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.post("/", authenticate, createTicket);
 
 export default router;
