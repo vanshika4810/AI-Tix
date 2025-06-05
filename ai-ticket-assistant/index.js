17a30fa84d53b0d2770fdc8c2a47a7b7afcc6e12
vanshika4810 <vanshikalal365@gmail.com>
2025-06-05 20:52:28 +0530

    ticketRoutes added, inngest's serve function added to invoke inngest

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import { serve } from "inngest/express";
 import userRoutes from "./routes/user";
+import ticketRoutes from "./routes/ticket";
+import { inngest } from "./inngest/client";
+import { onUserSignup } from "./inngest/functions/on-signup";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -10,6 +15,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
