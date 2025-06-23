5f25242698850a8f5038f2c4c8163e0c106c6426
vanshika4810 <vanshikalal365@gmail.com>
2025-06-23 13:44:40 +0530

    name updated

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ai-tix.app"], 
+    origin: ["http://localhost:5173", "https://ai-tix.vercel.app"], 
     credentials: true, 
   })
 );
